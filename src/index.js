7c74077ad6b0bb7f50d80a32d68cfcabe070196a
Frank Pagan <frank@cocreate.app>
2022-12-25 00:43:46 +0000

    feat: pass_to and pass_id now supports values, depreciated pass-value_to and pass-value_id

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import observer from '@cocreate/observer';
 import action from '@cocreate/actions';
-import passAttributes from "./passAttributes.js";
-import passValues from "./passValues.js";
-
-const CoCreatePass = {
-	...passAttributes, ...passValues
-};
 
 
 function init() {
 	__initPassSessionIds(); // will be derprciated for CoCreate-localStorage
+	let elements = document.querySelectorAll('[pass_id]');
+	initElements(elements);
+	window.addEventListener('storage', function(e) {
+		if (e.key == 'passedAttributes') {
+			elements = document.querySelectorAll('[pass_id]')
+			initElements(elements)
+		}
+	});
+
 }
 
 // ToDo: can be depreciated do to component localStorage
@@ -22,42 +25,139 @@ function __initPassSessionIds() {
 
 // ToDo: can be depreciateddo to component localStorage
 function __initPassItems(id, selector, noFetch) {
-	
 	if (id) {
 		let elements = document.querySelectorAll(selector);
 		elements.forEach(el => {
-			passAttributes._setAttributeValue(el, 'document_id', id);
-			passAttributes._setAttributeValue(el, 'fetch-document_id', id);
-			passAttributes._setAttributeValue(el, 'filter-value', id);
+			_setAttributeValue(el, 'document_id', id);
+			_setAttributeValue(el, 'fetch-document_id', id);
+			_setAttributeValue(el, 'filter-value', id);
 		});
 	}
 }
 
+function initElements (elements) {
+	for (let element of elements)
+		initElement(element);
+}
+
+function initElement (element) {
+	let pass_id = element.getAttribute('pass_id');
+	if (!pass_id) return;
+
+	let passedAttributes = window.localStorage.getItem('passedAttributes');
+	
+	passedAttributes = JSON.parse(passedAttributes);
+	if (!passedAttributes || passedAttributes.length == 0) return;
+	
+	let attrValues = passedAttributes[`'${pass_id}'`];
+	if (!attrValues) return;
+	_setAttributeValues(element, attrValues);
+}
+
+function _setAttributeValues (el, attrValues) {
+	let isRefresh = el.getAttribute('pass-refresh') || el.hasAttribute('pass-refresh');
+	if (isRefresh === 'false' || isRefresh === false || isRefresh === null || isRefresh === undefined)
+		isRefresh = false;
+	else
+		isRefresh = true;
+
+	Object.keys(attrValues).forEach(key => {
+		let attName = key.replace("'", '').replace("'", '');
+		_setAttributeValue(el, attName, attrValues[key], isRefresh);
+		_setAttributeValue(el, `pass-${attName}`, attrValues[key], isRefresh);
+		if (attName == 'collection' || attName == 'document_id' || attName == 'name'){
+			_setAttributeValue(el, `fetch-${attName}`, attrValues[key], isRefresh);
+			_setAttributeValue(el, `pass-fetch-${attName}`, attrValues[key], isRefresh);
+		}
+	});
+
+	// if (prefix) {
+	// 	_setAttributeValue(el, 'name', prefix + el.getAttribute('name'), isRefresh, true);
+	// 	_setAttributeValue(el, 'fetch-name', prefix + el.getAttribute('fetch-name'), isRefresh, true);
+	// 	_setAttributeValue(el, 'pass-prefix', prefix, isRefresh);
+	// }
+}
+
+function _setAttributeValue (element, attrname, value, isRefresh) {
+	// ToDo: if (value !== undefined)???
+	if (!element.getAttribute(attrname) || isRefresh || !element.getValue()) {
+		if (attrname == 'value') {					
+			if (element.value != undefined)
+				element.value = value;
+			else
+				element.setValue(value)
+		} else if (element.hasAttribute(attrname) && value)
+			element.setAttribute(attrname, value);
+	}
+}
+
+function passAttributes (element) {
+	let passedAttributes = {};
+	let elements = []
+
+	let form = element.closest('form');
+	if (form) {
+		elements = form.querySelectorAll('[pass_to]')
+	} else {
+		if (element.hasAttribute('pass_to'))
+			elements.push(element)
+		let nestedElements = element.querySelectorAll('[pass_to]')
+		elements.push(...nestedElements)
+	}
+
+	for (let i = 0; i < elements.length; i++) {
+		let attrValues = _getAttributeValues(elements[i]);
+		let pass_to = elements[i].getAttribute('pass_to');
+		Object.assign(passedAttributes, {[`'${pass_to}'`]: attrValues});
+		_getPassId(attrValues, pass_to);
+	}
+	
+	window.localStorage.setItem('passedAttributes', JSON.stringify(passedAttributes));
+
+	document.dispatchEvent(new CustomEvent('passEnd', {
+		detail: {}
+	}))
+
+}
+
+function _getAttributeValues (element) {
+	let attributeValues = {};
+	let attributes = element.attributes;
+	for (let attribute of attributes){
+		if (attribute.name.startsWith('pass-')) {
+			if (attribute.name == 'pass-value' && !attribute.value) 
+				Object.assign(attributeValues, {value: element.getvalue()});
+			else
+				Object.assign(attributeValues, {[`'${attribute.name.substring(5)}'`]: attribute.value});
+		}
+	}
+	return attributeValues;
+}
+
+function _getPassId(attrValues, pass_to) {
+	const elements = document.querySelectorAll(`[pass_id="${pass_to}"]`);
+	for (let element of elements)
+		_setAttributeValues(element, attrValues);
+}
+
+
 observer.init({
-	name: 'CoCreatepass',
+	name: 'CoCreatePass',
 	observe: ['addedNodes'],
 	target: '[pass_id]',
 	callback: function(mutation) {
-		CoCreatePass.initElement(mutation.target);
+		initElement(mutation.target);
 	}
 });
 
 action.init({
-	name: "passAttributes",
-	endEvent: "passAttributesEnd",
+	name: "pass",
+	endEvent: "passEnd",
 	callback: (btn, data) => {
-		CoCreatePass._setPassAttributes(btn);
-	},
-});
-
-action.init({
-	name: "passValues",
-	endEvent: "passValuesEnd",
-	callback: (btn, data) => {
-		CoCreatePass.passValueAction(btn);
+		passAttributes(btn);
 	},
 });
 
 init();
 
-export default CoCreatePass;
+export default {passAttributes};
